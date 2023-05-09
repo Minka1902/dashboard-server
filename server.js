0fd4abf9823a2e43d56e8015d82e500954b6fc4c
Michael Scharff <minka.scharff@gmail.com>
2023-05-09 10:53:12 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+
+
+// get Port, file path, folderName and mongoURI
+const { PORT = 4000,
+    // filePath = '../osm-proj/build/index.html',
+    // folderName = 'osm-proj',
+    mongoURI = "mongodb+srv://minka-epsg:kCCcG4k4LaD6DbEy@cluster0.khsprzk.mongodb.net/?retryWrites=true&w=majority" } = {};
+
+// include these before other routes
+app.options('*', cors());
+app.use(cors());
+
+// celebrate error handler
+app.use(errors());
+
+mongoose.connect(mongoURI)
+    .catch((err) => {
+        console.log(err);
+    });
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+
+});
+
+app.listen(PORT, function () {
+    console.log(`App is running on port ${PORT}`);
+});
